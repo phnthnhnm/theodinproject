976c484220f519173388544b9bec72c70c6c330f
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-12-05 14:35:42 +0700

    Add a working prototype, playable in the console

---
+++ full-stack-javascript/javascript/tic-tac-toe/script.js
@@ -0,0 +1,101 @@
+const gameBoard = (() => {
+  let board = ['', '', '', '', '', '', '', '', '']
+
+  const getBoard = () => board
+  const updateBoard = (index, marker) => {
+    if (board[index] === '') {
+      board[index] = marker
+      return true
+    }
+    return false
+  }
+  const resetBoard = () => {
+    board = ['', '', '', '', '', '', '', '', '']
+  }
+
+  return { getBoard, updateBoard, resetBoard }
+})()
+
+const Player = (name, marker) => {
+  const getName = () => name
+  const getMarker = () => marker
+
+  return { getName, getMarker }
+}
+
+const displayController = (() => {
+  const printBoard = () => {
+    const board = gameBoard.getBoard()
+    console.log(`
+      ${board[0]} | ${board[1]} | ${board[2]}
+      ---------
+      ${board[3]} | ${board[4]} | ${board[5]}
+      ---------
+      ${board[6]} | ${board[7]} | ${board[8]}
+    `)
+  }
+
+  const printResult = (result) => {
+    console.log(result)
+  }
+
+  return { printBoard, printResult }
+})()
+
+const gameController = (() => {
+  const player1 = Player('Player 1', 'X')
+  const player2 = Player('Player 2', 'O')
+  let currentPlayer = player1
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1
+  }
+
+  const checkWinner = () => {
+    const board = gameBoard.getBoard()
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    for (const pattern of winPatterns) {
+      const [a, b, c] = pattern
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return currentPlayer.getName()
+      }
+    }
+
+    return board.includes('') ? null : 'Draw'
+  }
+
+  const playRound = (index) => {
+    if (gameBoard.updateBoard(index, currentPlayer.getMarker())) {
+      const winner = checkWinner()
+      displayController.printBoard()
+      if (winner) {
+        displayController.printResult(winner === 'Draw' ? "It's a draw!" : `${winner} wins!`)
+        gameBoard.resetBoard()
+        displayController.printBoard()
+      } else {
+        switchPlayer()
+      }
+    } else {
+      console.log('Invalid move, try again.')
+    }
+  }
+
+  return { playRound }
+})()
+
+displayController.printBoard()
+gameController.playRound(0)
+gameController.playRound(1)
+gameController.playRound(4)
+gameController.playRound(2)
+gameController.playRound(8)
